614e8d9c15a655eab068b510ab3a8d1bc92f7157
vagrant <kennethracadio@hotmail.com>
2018-02-27 19:25:12 +0000

    Finished exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 7: Validations for both models
+
+
+print "Please enter your store's name:  "
+store_name = $stdin.gets.chomp.to_s
+
+t = Store.create(name: store_name)
+puts t.errors.full_messages
+
+
+
+
+
+
+
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,18 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validate :men_or_women_apparel
+  validates :name, length: { minimum: 3 }
+  # validates :annual_revenue, numericality: { only_integer: true,  }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def men_or_women_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "Must carry at lease one of the men's or women's apparel")
+      errors.add(:womens_apparel, "Must carry at lease one of the men's or women's apparel")
+    end
+  end
 end
+
+
+
