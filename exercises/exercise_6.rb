0d1edcbc1ffbe7abb4cc72714ea9a5cb3146561f
vagrant <kennethracadio@hotmail.com>
2018-02-27 18:04:46 +0000

    Finished exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 6: One-to-many association
+
+# We haven't used the Employee class (and employees table) at all yet.
+# If you look at this table's column structure, you'll find that it
+# has a `store_id` (integer) column. This is a column that identifies which
+# store each employee belongs to. It points to the `id` (integer) column of their store.
+
+# Let's tell Active Record that these two tables
+# are in fact related via the `store_id` column.
