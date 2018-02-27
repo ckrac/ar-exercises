00b594a73b1e08cd887d36039fead1acb7fa38b0
vagrant <kennethracadio@hotmail.com>
2018-02-27 17:54:08 +0000

    Finished exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 5: Calculations
+
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue of all stores: #{total_revenue}"
+total_stores = Store.all.size
+average_annual_revenue = total_revenue / total_stores
+puts "Average annual revenue: #{average_annual_revenue}"
+
+puts "Total stores generating over $1M or more: #{Store.where("annual_revenue > 1000000").size}"
